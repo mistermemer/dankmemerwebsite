fa7dfbafe3c93371de03d84c580b6e12573e7b29
Ronin <rxswyers@gmail.com>
2018-08-10 15:52:49 -0400

    Starting Table component

---
+++ src/app/Table/Table.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Table.css';
+
+class Table extends Component {
+  render() {
+    return(
+      <table></table>
+    )
+  }
+}
+
+export default Table;
