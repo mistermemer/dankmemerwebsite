6ef03cdd5473567a88338519b85f84fe6a27a6b6
Ronin <rxswyers@gmail.com>
2018-08-22 22:53:20 -0400

    Start Footer section
    Added social media links and link to the Discord server.
    Set up alignment
    *style is still up for debate*

--- src/app/Footer/Footer.jsx
+++ src/app/Footer/Footer.jsx
@@ -5,7 +5,25 @@ class Footer extends Component {
 render(){
     return(
       <footer className="footer">
-        <p>Created by the Dank Memer team.</p>
+        <div className="footer-column">
+          <p>Created by the Dank Memer team.</p>
+        </div>
+        <div className="footer-column">
+          <span className="footer-header">Social</span>
+          <div className="footer-content">
+            <a className="footer-link" href="https://github.com/Dank-Memer">GitHub</a>
+            <a className="footer-link" href="https://twitter.com/dankmemerbot">Twitter</a>
+            <a className="footer-link" href="https://discord.gg/9dsqMJ5" >Discord Server</a>
+          </div>
+        </div>
+        <div className="footer-column">
+          <span className="footer-header">Site</span>
+          <div className="footer-content">
+            <span>stuff</span>
+            <span>Other stuff</span>
+          </div>
+        </div>
+        
       </footer>      
     )
   }
