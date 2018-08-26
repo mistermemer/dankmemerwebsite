e6d2169762e2dde19d8fef9d8945048b06061680
Austin Melms <memyselfandlie@gmail.com>
2018-08-25 20:37:37 -0400

    endpoint to push commands from the bot

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const Express = require('express');
 const app = Express();
 const port = 3000;
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: true }))
 
 //Serve static files
 app.use('/', Express.static('./build/static'));
+
+app.post('/api/cmds', async (req, res) => {
+  console.log(res.body.cmds)
+})
 
 app.get('*', function (request, response){
   response.sendFile(`${__dirname}/build/static/index.html`);
