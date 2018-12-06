891de03d93f27eaab0b4ed6a6a0b089728f09c34
Austin Melms <austin@melmsie.com>
2018-12-06 10:21:23 -0500

    updated rules again

--- src/app/Pages/Rules/Rules.jsx
+++ src/app/Pages/Rules/Rules.jsx
@@ -12,9 +12,10 @@ class Rules extends Component {
         <h3 className="rules">By using Dank Memer, you agree to these rules.
         If you break any rules, we reserve the right to ban you from Dank Memer</h3>
         <p className="rules">Userbots, spamming, and macros are forbidden</p>
-        <p className="rules">Coin storage accounts/farming accounts (alts) are forbidden</p>
         <p className="rules">Sharing exploits is forbidden</p>
-        <p className="rules">Do not use the bot for racism or homophobia</p>
+        <p className="rules">Coin storage accounts/farming accounts (alts) are forbidden</p>
+        <p className="rules">Offensive/advertising names on the leaderboard will be wiped and blacklisted</p>
+        <p className="rules">Do not use the bot for racism, homophobia, or advertising</p>
       </div>
     )
   }
