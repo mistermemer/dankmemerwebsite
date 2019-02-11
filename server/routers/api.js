b42813bac83ec1cf0d3a75374e136734ee91b666
Austin Melms <memyselfandlie@gmail.com>
2019-02-11 00:37:16 -0500

    fix a little aethware

--- server/routers/api.js
+++ server/routers/api.js
@@ -1,6 +1,5 @@
 const { Router } = require('express');
 
-const dataDog = require('../datadog.js');
 const keys = require('../../keys.json');
 const { writeFileSync } = require('fs');
 const router = Router();
@@ -8,7 +7,7 @@ const router = Router();
 router.post('/cmds', (req, res) => {
   if (keys.includes(req.headers.authorization)) {
     console.log(req.body.commands);
-    fs.writeFileSync(`${__dirname}/../build/static/commands.json`, JSON.stringify(req.body.commands, '', '  '));
+    writeFileSync(`${__dirname}/../build/static/commands.json`, JSON.stringify(req.body.commands, '', '  '));
     commands = req.body.commands;
     res.status(200).json({ status: 'ok' });
   } else {
