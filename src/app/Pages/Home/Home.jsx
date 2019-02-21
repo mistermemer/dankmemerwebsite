7bc91be56aa921311b7cbc853207d2b7240e8bc4
Austin Melms <memyselfandlie@gmail.com>
2019-02-21 02:45:22 -0500

    make google happy

--- src/app/Pages/Home/Home.jsx
+++ src/app/Pages/Home/Home.jsx
@@ -10,14 +10,16 @@ class Home extends Component {
   }
   render() {
     return(
-      <div className="content">
-      <div className="home">
-        <img className="memer-img" height="250" width="250" src={MemerIMG} />
-        <span className="home-slogan">THE <span className="blurple">ORIGINAL</span> DISCORD MEME BOT</span>
-        <a href="https://goo.gl/ZHSsby"><Button content="Invite Now!" /></a>
-      </div>
-        
-      </div>
+      <main className="content">
+        <div className="home">
+          <img className="memer-img" alt="Dank Memer logo - the elusive and beautiful pepe" src={MemerIMG} />
+          <span className="home-slogan">THE <span className="blurple">ORIGINAL</span> DISCORD MEME BOT</span>
+          <div className="links">
+            <a href="https://getdankmemer.today"><Button content="Invite Bot" /></a>
+            <a href="https://getdankmemer.today"><Button content="Join Server" /></a>
+          </div>
+        </div>
+      </main>
     )
   }
 }
