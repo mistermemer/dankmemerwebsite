7a5a9f8ee37072d946d6ddd013ee2b8f62ee3186
Aetheryx <zain1@live.nl>
2019-03-30 00:21:07 +0100

    ew

--- server/util/db.js
+++ server/util/db.js
@@ -3,12 +3,12 @@ const { mongoURL } = require('../../config.json');
 
 module.exports = async () => {
   module.exports = await MongoClient
-  .connect(mongoURL, { useNewUrlParser: true })
-  .then(conn => conn.db('website'))
-  .then(db => db.collection('users'))
-  .catch(e => {
-    if (e.message.includes('ECONNREFUSED')) {
-      console.error('Failed to connect to MongoDB:', e.message);
-    }
-  });
+    .connect(mongoURL, { useNewUrlParser: true })
+    .then(conn => conn.db('website'))
+    .then(db => db.collection('users'))
+    .catch(e => {
+      if (e.message.includes('ECONNREFUSED')) {
+        console.error('Failed to connect to MongoDB:', e.message);
+      }
+    });
 };
