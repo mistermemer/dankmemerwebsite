34551cac5a3eefb7af695ffcfa719e13d60c8f89
aetheryx <zainalih3c@gmail.com>
2019-04-20 23:31:27 +0200

    add onClick param to Button

--- src/app/Components/Button/index.jsx
+++ src/app/Components/Button/index.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import './Button.scss';
 
-export default React.memo(({ link, children }) => {
+export default React.memo(({ link, children, onClick }) => {
   const button = (
-    <button className="button">
+    <button onClick={onClick} className="button">
       {children}
     </button>
   );
