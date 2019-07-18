6d9719202e31e5ba833fd1b1a8977c4d0fa4bd50
aetheryx <zainalih3c@gmail.com>
2019-07-18 18:28:18 +0200

    add admin panel for viewing payments

--- src/app/Pages/Admin/panels/GetPayment.jsx
+++ src/app/Pages/Admin/panels/GetPayment.jsx
@@ -1,9 +1,37 @@
 import React, { PureComponent } from 'react';
 import GenericPanel from './GenericPanel';
+import createModal from '../../../Components/Modal';
+import ReactJSON from 'react-json-view';
+import reactJSONProps from './reactJSONProps';
 
 export default class GetPayment extends PureComponent {
-  action (state) {
-    console.log(state);
+  async action (state) {
+    const res = await fetch(`/api/admin/findTransaction?${state.dropdownVal}=${state.textVal}`, {
+      credentials: 'same-origin',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(res => res.json())
+      .then(res => res.map(payment => ({
+        date: new Date(payment.times.create),
+        orderID: payment.orderID,
+        captureID: payment.captureID,
+        purchase: {
+          type: payment.item.name,
+          quantity: +payment.item.quantity,
+          total: payment.amount.total
+        },
+        payer: [ 'name', 'paypalEmail', 'discordEmail', 'paypalID', 'userID' ]
+          .reduce((data, key) => ({ ...data, [key]: payment.payer[key] }), {})
+      })));
+
+    createModal(
+      <ReactJSON
+        src={res}
+        { ...reactJSONProps }
+      />
+    );
   }
 
   render () {
@@ -12,8 +40,8 @@ export default class GetPayment extends PureComponent {
         title='Find Transaction'
         dropdownHeader='Parameter'
         textAreaHeader='Value'
-        defaultDropdown='appeal'
-        options={['Discord ID', 'PayPal ID', 'PayPal E-Mail', 'Payment ID']}
+        defaultDropdown='Discord ID'
+        options={['Discord ID', 'PayPal E-Mail', 'Full Name', 'Payment ID']}
         action={this.action.bind(this)}
         buttonText='Find'
       />
