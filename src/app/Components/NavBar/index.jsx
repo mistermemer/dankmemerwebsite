d67e629e5b327bf9d76ff90bc63a7cfd7f2a89a2
Nevvulo <bsward8@gmail.com>
2020-05-01 02:21:33 +1000

    style: fix linting errors

--- src/app/Components/NavBar/index.jsx
+++ src/app/Components/NavBar/index.jsx
@@ -1,53 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import './NavBar.css';
 
 import parseTime from '../../util/parseTime.js';
-let expanded = false;
 const NavBar = ({
   discount,
   login: { loggedIn, username, discriminator, isAdmin }
-}) => (
-  <nav className="navbar">
-    <span className="DM-nav">DANK MEMER</span>
-    <input class="navbar-btn" onChange={(e) => {
-      console.log('a', e)
-      expanded = !expanded;
-      if (expanded) {
-        e.target.parentElement.classList.add('navbar-expanded');
-      } else {
-        e.target.parentElement.classList.remove('navbar-expanded');
-      }
-    }} type="checkbox" id="navbar-btn" />
-    <label class="navbar-icon" for="navbar-btn"><span class="navicon"></span></label>
-    <ul className="nav-links">
-      <li className="nav-item">
-        <NavLink exact className="nav-link" activeClassName="active" to="/">HOME</NavLink>
-      </li>
-      <li className="nav-item">
-        <NavLink className="nav-link" activeClassName="active" to="/commands">COMMANDS</NavLink>
-      </li>
-      <li className="nav-item">
-        <NavLink className="nav-link" activeClassName="active" to="/about">ABOUT</NavLink>
-      </li>
-      {navigator.onLine && <li className="nav-item">
-        <NavLink className="nav-link" activeClassName="active" to="/blogs">BLOGS</NavLink>
-      </li>}
-      {navigator.onLine && <li className="nav-item">
-        <NavLink className="nav-link premium" activeClassName="active" to="/loot" data-discount={discount ? `FLASH SALE (${parseTime(discount.expiry - Date.now()).hours}H LEFT)` : ''}>LOOTBOXES</NavLink>
-      </li>}
-      {navigator.onLine && <span className="login">
-        {loggedIn ? (
-          <div className="user">
-              <span className="nav-link">{`${username.toUpperCase()}#${discriminator}`}</span>
-              <a className="nav-link login-button" href='/oauth/logout'>LOG OUT</a>
-          </div>
-        ) : (<a href="/oauth/login"><button className="obutton login-button">LOG IN</button></a>) }
-      </span>}
-    </ul>
-    
-  </nav>
-);
+}) => {
+  const [ navExpanded, setNavExpanded ] = useState(false);
+  return (
+    <nav className="navbar">
+      <span className="DM-nav">DANK MEMER</span>
+      <input className="navbar-btn" onChange={(e) => {
+        let expanded = !navExpanded;
+        setNavExpanded(expanded);
+        if (expanded) {
+          e.target.parentElement.classList.add('navbar-expanded');
+        } else {
+          e.target.parentElement.classList.remove('navbar-expanded');
+        }
+      }} type="checkbox" id="navbar-btn" />
+      <label className="navbar-icon" htmlFor="navbar-btn"><span className="navicon"></span></label>
+      <ul className="nav-links">
+        <li className="nav-item">
+          <NavLink exact className="nav-link" activeClassName="active" to="/">HOME</NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink className="nav-link" activeClassName="active" to="/commands">COMMANDS</NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink className="nav-link" activeClassName="active" to="/about">ABOUT</NavLink>
+        </li>
+        {navigator.onLine && <li className="nav-item">
+          <NavLink className="nav-link" activeClassName="active" to="/blogs">BLOGS</NavLink>
+        </li>}
+        {navigator.onLine && <li className="nav-item">
+          <NavLink className="nav-link premium" activeClassName="active" to="/loot" data-discount={discount ? `FLASH SALE (${parseTime(discount.expiry - Date.now()).hours}H LEFT)` : ''}>LOOTBOXES</NavLink>
+        </li>}
+        {navigator.onLine && <span className="login">
+          {loggedIn ? (
+            <div className="user">
+                <span className="nav-link">{`${username.toUpperCase()}#${discriminator}`}</span>
+                <a className="nav-link login-button" href='/oauth/logout'>LOG OUT</a>
+            </div>
+          ) : (<a href="/oauth/login"><button className="obutton login-button">LOG IN</button></a>) }
+        </span>}
+      </ul>
+      
+    </nav>
+  )
+};
 
 export default withRouter(connect(store => store)(NavBar));
